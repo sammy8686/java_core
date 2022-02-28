d494d480aaa4ba8e79f2af28872a06323ebf99d0
sammy8686 <100138578+sammy8686@users.noreply.github.com>
2022-02-28 10:29:56 +0530

    Add files via upload

---
+++ strg_fun16.java
@@ -0,0 +1,16 @@
+public class strg_fun16
+{
+ public static void main(String args[])
+ {
+ String s="janhavi";
+ String s1="janvi";
+  if(s.equals(s1)==true)
+  {
+  System.out.println("both strings are same");
+  }
+  else
+  {
+  System.out.println("both strings are not same");
+  }
+ }
+}
